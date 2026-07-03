53e3cd1a8ffb9f43046b9e24cc12cc85a6c9d6a3
Laura-githu <laura.bazan.paula@escola.pr.gov.br>
2026-07-03 14:38:21 +0000

    aula 5

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
@@ -12,12 +14,22 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function geraSenha(){
+    let senha = '' ;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
